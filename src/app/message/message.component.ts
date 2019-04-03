e13d80342e0a09975561f0916b9992477e3d8468
mucheniski <mucheniski@gmail.com>
2019-04-03 00:08:54 -0300

    13.9 Criando o componente de mensagem de erro de validacao

---
+++ src/app/message/message.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-message',
+  template: `
+  <div class="ui-message ui-messages-error" *ngIf="temErro()">
+    {{ text }}
+  </div>
+  `,
+  styles: [`
+    .ui-messages-error {
+      margin: 0;
+      margin-top: 4px;
+      border: 0;
+      color: red;
+    }
+  `]
+})
+export class MessageComponent {
+
+  @Input() error: string;
+  @Input()control: FormControl;
+  @Input()text: string;
+
+  temErro(): boolean {
+    return this.control.hasError(this.error) && this.control.dirty;
+  }
+
+}
