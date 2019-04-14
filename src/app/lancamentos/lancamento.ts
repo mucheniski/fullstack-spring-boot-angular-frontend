9833484a150f3758a2c4dbe9497abf222193173d
mucheniski <mucheniski@gmail.com>
2019-04-13 21:38:38 -0300

    Criacao das classes Lancamento, pessoa e categoria

---
+++ src/app/lancamentos/lancamento.ts
@@ -0,0 +1,14 @@
+import { Pessoa } from '../pessoas/pessoa';
+import { Categoria } from '../categorias/categoria';
+
+export class Lancamento {
+  codigo: number;
+  descricao: string;
+  dataVencimento: Date;
+  dataPagamento: Date;
+  valor: number;
+  observacao: string;
+  tipo = 'RECEITA';
+  categoria = new Categoria();
+  pessoa = new Pessoa();
+}
