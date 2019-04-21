91877bf30c3a71ed8de7bf0dc7afd1d83a6806f4
mucheniski <mucheniski@gmail.com>
2019-04-20 22:58:17 -0300

    19.15 Tratando acessos de usuarios deslogados no AuthGuard

--- src/app/seguranca/auth.guard.ts
+++ src/app/seguranca/auth.guard.ts
@@ -19,7 +19,20 @@ export class AuthGuard implements CanActivate {
     state: RouterStateSnapshot
   ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
-    if (next.data.roles && !this.auth.temQualquerPermissao(next.data.roles)) {
+    if (this.auth.tokenInvalido) {
+      console.log('Navegação com access token inválido. Obtendo novo token...');
+
+      return this.auth.refreshToken()
+              .then(() => {
+                if (this.auth.tokenInvalido()) {
+                  this.router.navigate(['/login']);
+                  return false;
+                }
+
+                return true;
+              });
+
+    } else if (next.data.roles && !this.auth.temQualquerPermissao(next.data.roles)) {
       this.router.navigate(['/nao-autorizado']);
       return false;
     }
