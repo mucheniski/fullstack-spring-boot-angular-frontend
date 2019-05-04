494b0d3cc75008eba82e51c1e5c1744015238cf9
mucheniski <mucheniski@gmail.com>
2019-05-04 16:39:50 -0300

    23.02 Plotanfo graficos com dados estaticos

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { PanelModule } from 'primeng/panel';
+import { ChartModule } from 'primeng/chart';
+
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -9,6 +12,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
+
+    PanelModule,
+    ChartModule,
 
     SharedModule,
     DashboardRoutingModule
